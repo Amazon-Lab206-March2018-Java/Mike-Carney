2aa93150e924455ec3c9bc9c49a2827c9815a9d2
Carney <micarney@amazon.com>
2018-04-23 11:18:02 -0700

    first commit

---
+++ Fundamentals/FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args){
+        System.out.println("my name is Mike");
+        System.out.println("I am 38 years old");
+        System.out.println("My hometown is Denver,CO");
+    }
+}
